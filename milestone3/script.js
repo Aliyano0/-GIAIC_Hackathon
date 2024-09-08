31f86c5024c6ee39f59f578d7ac8245c81d40549
Aliyano0 <aliyanaqeel97@gmail.com>
2024-09-08 12:43:52 +0500

    Finished Dynamic Resume Builder

--- milestone3/script.js
+++ milestone3/script.js
@@ -13,7 +13,7 @@ function workExpExpander() {
     workExpBtn.addEventListener("click", function () {
         clutter += `<label class="labels" for="company">Enter Your Company Name: </label>
                   <br>
-                  <input class="inputs" type="text" placeholder="Intstitution" id="company">
+                  <input class="inputs" type="text" placeholder="Company" id="company">
                   <br>
                   <label class="labels" for="position">Enter Your Position: </label>
                   <br>
@@ -26,7 +26,7 @@ function workExpExpander() {
                   <br>
                   <label class="labels date-labels" for="edate">Input End Date: </label>
                   <br class="display-hidden">
-                  <input class="workdates" type="date" name="enddate" id="edate">
+                  <input class="workdates" type="date" name="enddate" id="work-edate">
                   <br>
                   <label class="labels" for="work-desc">Description: </label>
                   <br>
@@ -75,6 +75,122 @@ function educationExpander() {
     });
 }
 ;
+// Interactivity ends here // 
+// Form Input starts here  //
+function generateResume() {
+    document.getElementById('resume-form')?.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const resumeContainer = document.querySelector(".resume-container");
+        resumeContainer.style.borderColor = "rgba(125, 186, 92, 0.5)";
+        // Get the input values
+        // Personal Information
+        const firstNameElement = document.getElementById("fname");
+        const lastNameElement = document.getElementById("lname");
+        const emailElement = document.getElementById("email");
+        const addressElement = document.getElementById("address");
+        const phoneNumberElement = document.getElementById("phnumber");
+        const profilePicElement = document.getElementById("resume-image");
+        // Education Input
+        const educationInstitutionElement = document.getElementById("institution");
+        const educationDegreeElement = document.getElementById("degree");
+        const educationStartDateElement = document.getElementById("sdate");
+        const educationEndDateElement = document.getElementById("edate");
+        // Work Experience Input
+        const experienceCompanyElement = document.getElementById("company");
+        const experiencePositionElement = document.getElementById("position");
+        const experienceWorkSdateElement = document.getElementById("work-sdate");
+        const experienceWorkEdateElement = document.getElementById("work-edate");
+        const experienceWorkDescElement = document.getElementById("work-desc");
+        // Skills Input
+        const skillsNameElement = document.getElementById("skill");
+        const skillLvlElement = document.getElementById("skill-lvl");
+        if (firstNameElement && lastNameElement && emailElement && addressElement && phoneNumberElement && profilePicElement && educationInstitutionElement && educationDegreeElement && educationStartDateElement && educationEndDateElement && experienceCompanyElement && experiencePositionElement && experienceWorkSdateElement && experienceWorkEdateElement && experienceWorkDescElement && skillLvlElement && skillsNameElement) {
+            // Getting values of Elements:
+            const name = firstNameElement.value + " " + lastNameElement.value;
+            const email = emailElement.value;
+            const address = addressElement.value;
+            const phnumber = phoneNumberElement.value;
+            const profilePictureFile = profilePicElement.files?.[0];
+            const profilePictureURL = profilePictureFile ? URL.createObjectURL(profilePictureFile) : '';
+            const educationInstitution = educationInstitutionElement.value;
+            const educationDegree = educationDegreeElement.value;
+            const educationStartDate = educationStartDateElement.value;
+            const educationEndDate = educationEndDateElement.value;
+            const experienceCompany = experienceCompanyElement.value;
+            const experiencePosition = experiencePositionElement.value;
+            const experienceWorkSdate = experienceWorkSdateElement.value;
+            const experienceWorkEdate = experienceWorkEdateElement.value;
+            const experienceWorkDesc = experienceWorkDescElement.value;
+            const skillName = skillsNameElement.value;
+            const skillLvl = skillLvlElement.value;
+            // Update the resume preview
+            const resumeContent = `
+            <div class="heading-display">
+                <h1 class="heading-text-display">Resume</h1>
+            </div>
+
+            <div class="main-content">
+
+
+                <div class="img-cont">
+                <!-- <img class="${profilePictureURL}" src="" alt=""> -->
+                </div>
+
+
+                <div class="contactinfo-display">
+                <h2 class="display-headings">Contact Information:</h2>
+                <p class="display-paragraphs">Name: ${name}</p>
+                <p class="display-paragraphs">Phone Number: ${phnumber}</p>
+                <p class="display-paragraphs">Email: ${email}</p>
+                <p class="display-paragraphs">Address: ${address}</p>
+                </div>
+
+
+                <div class="education-display">
+                <h2 class="display-headings">Education: </h2>
+                <ul>
+                    <li class="display-lis">Institution: ${educationInstitution}</li>
+                    <li class="display-lis">Degree: ${educationDegree}</li>
+                    <li class="display-lis">Start Date: ${educationStartDate}</li>
+                    <li class="display-lis">End Date: ${educationEndDate}</li>
+                </ul>
+                </div>
+
+
+                <div class="workexp-display">
+                <h2 class="display-headings">Work Experience:</h2>
+                <ul>
+                    <li class="display-lis">Company Name: ${experienceCompany}</li>
+                    <li class="display-lis">Position: ${experiencePosition}</li>
+                    <li class="display-lis">Start Date: ${experienceWorkSdate}</li>
+                    <li class="display-lis">End Date: ${experienceWorkEdate}</li>
+                    <li class="display-lis">Description: ${experienceWorkDesc}</li>
+                </ul>
+                </div>
+
+
+                <div class="skills-display">
+                <h2 class="display-headings">Skills:</h2>
+                <ul>
+                    <li class="display-lis">Skill: ${skillName}</li>
+                    <li class="display-lis">Skill Level: ${skillLvl}</li>
+                </ul>
+                </div>
+
+
+            </div>
+        `;
+            const resumeContainer = document.querySelector(".resume-container");
+            resumeContainer.innerHTML = resumeContent;
+        }
+        else {
+            console.error('One or More Output Elements are Missing');
+        }
+        ;
+    });
+}
+;
 educationExpander();
 skillsExpander();
 workExpExpander();
+generateResume();
--- milestone3/script.ts
+++ milestone3/script.ts
@@ -15,7 +15,7 @@ function workExpExpander(){
     workExpBtn.addEventListener("click", function(){
      clutter += `<label class="labels" for="company">Enter Your Company Name: </label>
                   <br>
-                  <input class="inputs" type="text" placeholder="Intstitution" id="company">
+                  <input class="inputs" type="text" placeholder="Company" id="company">
                   <br>
                   <label class="labels" for="position">Enter Your Position: </label>
                   <br>
@@ -28,7 +28,7 @@ function workExpExpander(){
                   <br>
                   <label class="labels date-labels" for="edate">Input End Date: </label>
                   <br class="display-hidden">
-                  <input class="workdates" type="date" name="enddate" id="edate">
+                  <input class="workdates" type="date" name="enddate" id="work-edate">
                   <br>
                   <label class="labels" for="work-desc">Description: </label>
                   <br>
@@ -75,8 +75,141 @@ function educationExpander(){
     education.innerHTML = clutter
 })};
 
+// Interactivity ends here // 
+
+// Form Input starts here  //
+
+function generateResume() {
+    document.getElementById('resume-form')?.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const resumeContainer = document.querySelector(".resume-container") as HTMLDivElement;
+        resumeContainer.style.borderColor = "rgba(125, 186, 92, 0.5)"
+        // Get the input values
+            
+        // Personal Information
+        const firstNameElement = document.getElementById("fname") as HTMLInputElement;
+        const lastNameElement = document.getElementById("lname") as HTMLInputElement;
+        const emailElement = document.getElementById("email") as HTMLInputElement;
+        const addressElement = document.getElementById("address") as HTMLInputElement;
+        const phoneNumberElement = document.getElementById("phnumber") as HTMLInputElement;
+        const profilePicElement = document.getElementById("resume-image") as HTMLInputElement;
+
+        // Education Input
+        const educationInstitutionElement = document.getElementById("institution") as HTMLInputElement;
+        const educationDegreeElement = document.getElementById("degree") as HTMLInputElement;
+        const educationStartDateElement = document.getElementById("sdate") as HTMLInputElement;
+        const educationEndDateElement = document.getElementById("edate") as HTMLInputElement;
+
+        // Work Experience Input
+        const experienceCompanyElement = document.getElementById("company") as HTMLInputElement;
+        const experiencePositionElement = document.getElementById("position") as HTMLInputElement;
+        const experienceWorkSdateElement = document.getElementById("work-sdate") as HTMLInputElement;
+        const experienceWorkEdateElement = document.getElementById("work-edate") as HTMLInputElement;
+        const experienceWorkDescElement = document.getElementById("work-desc") as HTMLInputElement;
+
+        // Skills Input
+        const skillsNameElement = document.getElementById("skill") as HTMLInputElement;
+        const skillLvlElement = document.getElementById("skill-lvl") as HTMLInputElement;
+
+        if(firstNameElement && lastNameElement && emailElement && addressElement && phoneNumberElement && profilePicElement && educationInstitutionElement && educationDegreeElement && educationStartDateElement && educationEndDateElement && experienceCompanyElement && experiencePositionElement && experienceWorkSdateElement && experienceWorkEdateElement && experienceWorkDescElement && skillLvlElement && skillsNameElement){
+
+
+
+        
+        // Getting values of Elements:
+        const name = firstNameElement.value + " " + lastNameElement.value;
+        const email = emailElement.value;
+        const address = addressElement.value;
+        const phnumber = phoneNumberElement.value;
+        const profilePictureFile = profilePicElement.files?.[0]
+        const profilePictureURL = profilePictureFile ? URL.createObjectURL(profilePictureFile) : '';  
+        const educationInstitution = educationInstitutionElement.value; 
+        const educationDegree = educationDegreeElement.value; 
+        const educationStartDate = educationStartDateElement.value; 
+        const educationEndDate = educationEndDateElement.value; 
+        const experienceCompany = experienceCompanyElement.value;
+        const experiencePosition = experiencePositionElement.value;
+        const experienceWorkSdate = experienceWorkSdateElement.value;
+        const experienceWorkEdate = experienceWorkEdateElement.value;
+        const experienceWorkDesc = experienceWorkDescElement.value;
+        const skillName = skillsNameElement.value;
+        const skillLvl = skillLvlElement.value;
+        // Update the resume preview
+        const resumeContent = `
+            <div class="heading-display">
+                <h1 class="heading-text-display">Resume</h1>
+            </div>
+
+            <div class="main-content">
+
+
+                <div class="img-cont">
+                <!-- <img class="${profilePictureURL}" src="" alt=""> -->
+                </div>
+
+
+                <div class="contactinfo-display">
+                <h2 class="display-headings">Contact Information:</h2>
+                <p class="display-paragraphs">Name: ${name}</p>
+                <p class="display-paragraphs">Phone Number: ${phnumber}</p>
+                <p class="display-paragraphs">Email: ${email}</p>
+                <p class="display-paragraphs">Address: ${address}</p>
+                </div>
+
+
+                <div class="education-display">
+                <h2 class="display-headings">Education: </h2>
+                <ul>
+                    <li class="display-lis">Institution: ${educationInstitution}</li>
+                    <li class="display-lis">Degree: ${educationDegree}</li>
+                    <li class="display-lis">Start Date: ${educationStartDate}</li>
+                    <li class="display-lis">End Date: ${educationEndDate}</li>
+                </ul>
+                </div>
+
+
+                <div class="workexp-display">
+                <h2 class="display-headings">Work Experience:</h2>
+                <ul>
+                    <li class="display-lis">Company Name: ${experienceCompany}</li>
+                    <li class="display-lis">Position: ${experiencePosition}</li>
+                    <li class="display-lis">Start Date: ${experienceWorkSdate}</li>
+                    <li class="display-lis">End Date: ${experienceWorkEdate}</li>
+                    <li class="display-lis">Description: ${experienceWorkDesc}</li>
+                </ul>
+                </div>
+
+
+                <div class="skills-display">
+                <h2 class="display-headings">Skills:</h2>
+                <ul>
+                    <li class="display-lis">Skill: ${skillName}</li>
+                    <li class="display-lis">Skill Level: ${skillLvl}</li>
+                </ul>
+                </div>
+
+
+            </div>
+        `;
+    
+        const resumeContainer = document.querySelector(".resume-container") as HTMLDivElement;
+
+        resumeContainer.innerHTML = resumeContent;
+        } else{
+        console.error('One or More Output Elements are Missing');
+    };
+})};
+  
+
+
+
+
+
+
 
 
 educationExpander();
 skillsExpander();
 workExpExpander();
+generateResume();
